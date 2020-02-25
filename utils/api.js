982a34c9302e37abd01d0ae557628c1eeff6c9c2
HamedKh99 <goo.hamedacc@gmail.com>
2020-02-25 23:30:12 +0330

    base addr production

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,5 @@
-export const BASE_ADDRESS = 'http://127.0.0.1:8000'
+export const BASE_ADDRESS = 'http://37.152.183.218/api/'
+// export const BASE_ADDRESS = 'http://127.0.0.1:8000'
 export const HOME = `${BASE_ADDRESS}/homepage/`
 export const HAFTKHAN_PRE = `${BASE_ADDRESS}/events/haftkhanpre`
 export const HAFTKHAN_SIGNUP = `${BASE_ADDRESS}/events/haftkhansignup/`
