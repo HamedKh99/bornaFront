377cb1fb69934da0ca45d222bb267d29b4d05057
HamedKh99 <goo.hamedacc@gmail.com>
2020-04-04 22:07:34 +0430

    fixed api

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 const isDev = !process.env.NOD_ENV || process.env.NOD_ENV === 'development'
 
-export const BASE_IP = (isDev ? 'http://127.0.0.1:8000' : 'http://37.152.183.218')
-export const BASE_ADDRESS = (isDev ? 'http://127.0.0.1:8000' : 'http://37.152.183.218/api/')
+export const BASE_IP = (isDev ? 'http://127.0.0.1:8000' : 'https://bornains.ir')
+export const BASE_ADDRESS = (isDev ? 'http://127.0.0.1:8000' : 'https://bornains.ir/api')
 
 export const HOME = `${BASE_ADDRESS}/homepage/`
 export const HAFTKHAN_PRE = `${BASE_ADDRESS}/events/haftkhanpre`
